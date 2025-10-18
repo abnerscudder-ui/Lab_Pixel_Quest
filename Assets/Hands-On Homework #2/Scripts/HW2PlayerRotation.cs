99acaf332ef42203d192409191f0005dc75d1f59
abnerscudder-ui <abnerscudder@gmail.com>
2025-10-18 12:28:55 -0400

    zxcfgfdcvbg

--- Assets/Hands-On Homework #2/Scripts/HW2PlayerRotation.cs
+++ Assets/Hands-On Homework #2/Scripts/HW2PlayerRotation.cs
@@ -2,5 +2,12 @@ using UnityEngine;
 
 public class HW2PlayerRotation : MonoBehaviour
 {
+    private Camera _camera;
 
+    private string Cameraname = "Game Camera";
+
+    private void Start()
+    {
+        _camera = GameObject.Find(Cameraname).GetComponent<Camera>();
+    }
 }
---
+++ Assets/Hands-On Homework #2/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2D;
+
+    private float _xSpeed;
+    private float _ySpeed;
+
+    public float Speed = 3;
+
+    private string InputX = "Horizontal";
+    private string InputY = "Vertical";
+
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        _xSpeed = Input.GetAxis(InputX);
+        _ySpeed = Input.GetAxis(InputY);
+
+        _rigidbody2D.velocity = new Vector2(_xSpeed, _ySpeed) * Speed;
+    }
+}
