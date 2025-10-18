83e1d04e8427d1635efa441e3c239bd36293c9ee
abnerscudder-ui <abnerscudder@gmail.com>
2025-10-18 13:18:37 -0400

    Update HW2PlayerRotation.cs

--- Assets/Hands-On Homework #2/Scripts/HW2PlayerRotation.cs
+++ Assets/Hands-On Homework #2/Scripts/HW2PlayerRotation.cs
@@ -6,8 +6,18 @@ public class HW2PlayerRotation : MonoBehaviour
 
     private string Cameraname = "Game Camera";
 
+    private Vector3 _positionMouse;
+
     private void Start()
     {
         _camera = GameObject.Find(Cameraname).GetComponent<Camera>();
     }
+
+    private void Update()
+    {
+       _positionMouse = _camera.ScreenToWorldPoint(Input.mousePosition);  
+
+        Vector3 pos = _positionMouse - transform.position;
+    }
+
 }
